2fbfa4bcecd5d55edcd6c13d112cd4ea60345397
Teodor Grigore <grigore.teodor9@gmail.com>
2025-10-30 01:34:34 +0200

    test workflows

--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -297,7 +297,7 @@ func TestNextTokenOnTwoCharacterTokens(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokenType is wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokenType is wrong.expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - tokenLiteral is wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
